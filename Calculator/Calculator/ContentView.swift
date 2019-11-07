1712883fc575fbd5da83f9124d7101e8f4dc23c6
LiRui <bskyrui@msn.cn>
2019-11-07 18:41:21 +0800

    update readme & add HStack to subviews

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -9,19 +9,62 @@
 import SwiftUI
 
 
-// 将把当前的 View 包裹在一个新的 View 里，并在四周填充系统默认尺寸的空白, .padding(.top, 16)
+// padding()将把当前的 View 包裹在一个新的 View 里，并在四周填充系统默认尺寸的空白, .padding(.top, 16)
+
+struct CalculatorBtn : View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    
+    var body: some View {
+        return Button(action: action) {
+            Text(title)
+                .font(.system(size: fontSize))  // 设置字体
+                .foregroundColor(.white)        // 设定文本的颜色
+                .frame(width: size.width, height: size.height)
+                .background(Color(backgroundColorName))
+                .cornerRadius(size.width / 2)
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Button(action: {
-            print("Button: +")
-        }) {
-            Text("+")
-            .font(.system(size: 38))   // 设置字体
-            .foregroundColor(.white)   // 设定文本的颜色
-            .frame(width: 88, height: 88)
-            .background(Color("operatorBackground"))
-            .cornerRadius(44)
+        HStack {
+            CalculatorBtn(
+                title: "1",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "digitBackground"
+            ) {
+                print("Button: 1")
+            }
+            
+            CalculatorBtn(
+                title: "2",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "digitBackground"
+            ) {
+               print("Button: 2")
+            }
+           
+            CalculatorBtn(
+                title: "3",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "digitBackground"
+            ) {
+                print("Button: 3")
+            }
+            
+            CalculatorBtn(
+                title: "+",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "operatorBackground"
+            ) {
+                print("Button: +")
+            }
+            
         }
     }
 }
--- readme.md
+++ readme.md
@@ -6,3 +6,24 @@
 2. 选择 iOS tab 下的 Single View App 模板
 3. 接下来，将项目命名为 SwiftUIDemo，并且勾选 "Use SwiftUI"
 4. ContentView.swift 中 ContentView 为实际的 UI
+
+
+> 提取为子View
+```swift
+// 按住 Command 并单击 Button，在弹出的菜单中选择 "Extract Subview"，可以将这个 Button 提取为一个新的 View
+var body: some View {
+    HStack {
+        Button(action: {
+            print("Button: 1")
+        }) {
+            Text("1")
+                .font(.system(size: 38))
+                .foregroundColor(.white)
+                .frame(width: 88, height: 88)
+                .background(Color("digitBackground"))
+                .cornerRadius(44)
+        }
+        // ...
+    }
+}
+```
