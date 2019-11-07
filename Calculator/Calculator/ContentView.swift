4c22665d578a0e617f6f65cc4393f0475981551c
LiRui <bskyrui@msn.cn>
2019-11-07 16:09:56 +0800

    create calc project

---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Rui Li on 2019/11/7.
+//  Copyright © 2019 Rui Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
